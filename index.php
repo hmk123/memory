5246ce85958982671ed2b9d6bc9f187d76740f77
hmk123 <917811829@qq.com>
2021-12-03 22:48:00 +0800

    php

---
+++ index.php
@@ -0,0 +1,113 @@
+
+
+
+<?php
+
+header( 'Content-Type:text/html;charset=utf-8 '); 
+$servername = "localhost";
+$username = "root";
+$password = "hmk123";
+$dbname = "test";
+ 
+
+
+$arr = array('a'=>1,'b'=>2);
+
+exit(json_encode($arr));
+ 
+
+return;
+
+
+
+
+$con=mysqli_connect("localhost",$username,$password,$dbname);
+// 检测连接
+if (mysqli_connect_errno())
+{
+        echo "连接失败: " . mysqli_connect_error();
+}
+
+mysqli_query($con,"UPDATE myguests SET email='9178@qq.com',lastname='hmk'
+WHERE LastName='Doe'");
+
+mysqli_close($con);
+
+
+
+
+
+
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+ 
+
+
+
+
+
+
+
+//MySQL 读取数据
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+ 
+if ($result->num_rows > 0) {
+    // 输出数据
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 结果";
+}
+$conn->close();
+
+
+
+
+
+return;
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password, $dbname);
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+} 
+ 
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+ 
+if ($conn->query($sql) === TRUE) {
+    echo "新记录插入成功";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+ 
+$conn->close();
+
+return;
+
+
+// 使用 sql 创建数据表
+$sql = "CREATE TABLE MyGuests (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL,
+    email VARCHAR(50),
+    reg_date TIMESTAMP
+    )";
+     
+    if ($conn->query($sql) === TRUE) {
+        echo "Table MyGuests created successfully";
+    } else {
+        echo "创建数据表错误: " . $conn->error;
+    }
+     
+    $conn->close();
+    ?>
---
+++ 文件加密/文件加密/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 文件加密
+{
+    class Program
+    {
+
+           // 加密方法：将原字符串与加密字符进行异或
+        private static char[] Encrypt(string content, string secretKey)
+        {
+
+            char[] data = content.ToCharArray();
+            char[] key = secretKey.ToCharArray();
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] ^= key[i % key.Length];
+            }
+
+            return data;
+        }
+        //解密方法：将加密后的字符数组与加密字符进行异或
+        private static string Decrypt(char[] data, string secretKey)
+        {
+            char[] key = secretKey.ToCharArray();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] ^= key[i % key.Length];
+            }
+
+            return new string(data);
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            string filePath = Directory.GetCurrentDirectory();
+            filePath = filePath + "\\test.txt";                  //得到路径
+            string str = File.ReadAllText(filePath, Encoding.UTF8);
+
+            char[] data = Encrypt(str, "123");      //将"123"作为密钥
+             string afterEncrypt = new string(data);
+            File.WriteAllText(filePath, afterEncrypt, Encoding.UTF8);
+
+            
+            // char[] data1 = str.ToCharArray();
+           // string strl = Decrypt(data1, "123");    //解密 str1为解密后的文本
+
+
+
+            Console.WriteLine(str);
+           // Console.WriteLine(strl);
+            Console.ReadKey();
+
+        }
+
+    
+
+    }
+}
